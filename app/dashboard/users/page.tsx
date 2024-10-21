dc86f9ba236114a9af5687ed7dc46129aee8c505
MrSanty <sr.santyquinteror@gmail.com>
2024-10-21 01:51:02 -0500

    Refactor User component to use QueryClientProvider for React Query integration and update TableData component to fetch users based on company_id

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,8 +1,52 @@
-import { Development } from "@/components"
+import { auth } from "@/auth"
+import { TableData } from "./ui/TableData"
 
-const User = () => {
+const User = async () => {
+  const session = await auth()
+
   return (
-    <Development />
+    <>
+      <div className="bg-zinc-50 p-5 rounded-md">
+        <div className="flex justify-between items-center mb-6">
+          <h2 className="text-2xl font-bold">Usuarios</h2>
+          {/* <CreateModal
+            addUser={addUser}
+            className="sm:hidden"
+          /> */}
+        </div>
+        <div className="flex justify-between items-center mb-3">
+          {/* <div className="flex items-start flex-col sm:flex-row sm:items-center gap-1">
+            <Input
+              isClearable
+              className="w-full"
+              variant="bordered"
+              placeholder="Buscar usuario"
+              size="sm"
+              startContent={<SearchIcon className="size-4" />}
+              value={search}
+              onClear={() => onClear()}
+              onValueChange={onSearchChange}
+            />
+          </div>
+
+          <CreateModal
+            addUser={addUser}
+            className="hidden sm:flex"
+          /> */}
+        </div>
+        <div className="overflow-x-auto rounded-md">
+          {/* <TableData
+            userLogged={userData.email}
+            data={tempData}
+            editUser={editUser}
+            deleteUser={deleteUser}
+          /> */}
+          <TableData
+            company_id={session?.user.company_id as number}
+          />
+        </div>
+      </div>
+    </>
   )
 }
 export default User
---
+++ app/dashboard/users/ui/TableData.tsx
@@ -0,0 +1,108 @@
+'use client'
+
+import { getUsers } from "@/actions/user/getUsers"
+import { useQuery } from "@tanstack/react-query"
+import toast from "react-hot-toast"
+import {
+  Table,
+  TableHeader,
+  TableBody,
+  TableColumn,
+  TableRow,
+  TableCell,
+  Spinner
+} from "@nextui-org/react"
+import { FC } from "react"
+
+interface TableDataProps {
+  company_id: number
+}
+
+export const TableData: FC<TableDataProps> = ({ company_id }) => {
+  const { data, isLoading } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      try {
+        const data = await getUsers(company_id)
+        return data
+      } catch (error) {
+        toast.error("Error al obtener los usuarios")
+        return []
+      }
+    },
+  })
+
+  return (
+    <Table
+      radius="md"
+      isStriped
+    >
+      <TableHeader>
+        <TableColumn
+          align="center"
+        >
+          Nombre
+        </TableColumn>
+        <TableColumn align="center">
+          Email
+        </TableColumn>
+        <TableColumn align="center">
+          Tipo de Documento
+        </TableColumn>
+        <TableColumn align="center">
+          Número de Documento
+        </TableColumn>
+        <TableColumn align="center">
+          Rol
+        </TableColumn>
+        {/* <TableColumn align="end">
+          Acciones
+        </TableColumn> */}
+      </TableHeader>
+      <TableBody
+        emptyContent="No hay datos"
+        loadingState={isLoading ? "loading" : "idle"}
+        loadingContent={<Spinner size="sm" />}
+      >
+        {
+          data?.map((item, index) => (
+            <TableRow key={index}>
+              <TableCell>
+                {item.first_name} {item.last_name}
+              </TableCell>
+              <TableCell>
+                {item.email}
+              </TableCell>
+              <TableCell>
+                {item.type_doc}
+              </TableCell>
+              <TableCell>
+                {item.num_doc}
+              </TableCell>
+              <TableCell>
+                Administrador
+              </TableCell>
+              {/* <TableCell
+                className="flex justify-end gap-2"
+              >
+                <EditModal
+                  data={item}
+                  editUser={editUser}
+                />
+
+                {
+                  userLogged !== item.email && (
+                    <DeleteModal
+                      id={`${item.id}`}
+                      deleteUser={deleteUser}
+                    />
+                  )
+                }
+              </TableCell> */}
+            </TableRow>
+          )) || []
+        }
+      </TableBody>
+    </Table>
+  )
+}
