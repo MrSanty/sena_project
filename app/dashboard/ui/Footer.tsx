b43ab238a051014000ef7f14346b236cc1a79ac6
MrSanty <sr.santyquinteror@gmail.com>
2024-10-21 00:14:29 -0500

    Add Footer component for displaying copyright information

---
+++ app/dashboard/ui/Footer.tsx
@@ -0,0 +1,11 @@
+export const Footer = ({ }) => {
+  return (
+    <footer className="footer bg-zinc-50 text-center p-4">
+      <p
+        className="text-xs font-semibold"
+      >
+        &copy; {new Date().getFullYear()} todos los derechos reservados por Centro de Automatización Industrial SENNOVA - Regional Caldas
+      </p>
+    </footer>
+  )
+}
