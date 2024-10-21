a49e9bddea31962c07dd218f8f568af7fcc90041
MrSanty <sr.santyquinteror@gmail.com>
2024-10-21 00:14:20 -0500

    Add Providers component for managing session and sidebar state

---
+++ app/dashboard/providers.tsx
@@ -0,0 +1,35 @@
+'use client'
+import { SideBarContext } from "@/hooks"
+import { SessionProvider } from "next-auth/react";
+import { FC, useState } from "react"
+
+export const Providers: FC<{ children: React.ReactNode }> = ({
+  children
+}) => {
+  const [ isSidebarOpen, setIsSidebarOpen ] = useState(false);
+  const [ isDrawer, setIsDrawer ] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen)
+  }
+
+  const toggleDrawer = () => {
+    setIsDrawer(!isDrawer)
+  }
+
+
+  return (
+    <SessionProvider>
+      <SideBarContext.Provider
+        value={{
+          isSidebarOpen,
+          toggleSidebar,
+          isDrawer,
+          toggleDrawer
+        }}
+      >
+        {children}
+      </SideBarContext.Provider>
+    </SessionProvider>
+  )
+}
