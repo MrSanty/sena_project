14e129dfc9f7f4697a08acdaf5eacbf714fc6514
MrSanty <sr.santyquinteror@gmail.com>
2024-10-21 00:15:37 -0500

    Add DashboardLayout component for the dashboard page

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next"
+import { FC } from "react"
+import { Providers } from "./providers";
+import { SideBar } from "./ui/SideBar";
+import { Navbar } from "./ui/Navbar";
+import { Footer } from "./ui/Footer";
+
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+  description: "Dashboard page",
+}
+
+interface DashboardLayoutProps {
+  children: React.ReactNode;
+}
+
+const DashboardLayout: FC<DashboardLayoutProps> = ({ children }) => {
+  return (
+    <div
+      className="max-h-screen bg-zinc-200/50 grid-container"
+    >
+      <Providers>
+        <SideBar />
+        <Navbar />
+      </Providers>
+      <main
+        className="main py-5 px-2 lg:px-5"
+      >
+        {children}
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default DashboardLayout
