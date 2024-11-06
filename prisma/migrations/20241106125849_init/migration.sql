f430bd85be5c8d9e800da5a1b8e8cef405fd3e29
MrSanty <sr.santyquinteror@gmail.com>
2024-11-06 08:00:01 -0500

    Make 'description' in stock and 'first_name', 'last_name' in users required

---
+++ prisma/migrations/20241106125849_init/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - Made the column `description` on table `stock` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `first_name` on table `users` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `last_name` on table `users` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "stock" ALTER COLUMN "description" SET NOT NULL;
+
+-- AlterTable
+ALTER TABLE "users" ALTER COLUMN "first_name" SET NOT NULL,
+ALTER COLUMN "last_name" SET NOT NULL;
